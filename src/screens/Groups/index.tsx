5fb4f752bd4c06d8bff9a7df093a433ff5632236
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-10 17:49:35 -0300

    CSS-in-JS - Biblioteca de ícones

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '@components/Header';
+import * as S from './styles'
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <S.Container>
+      <Header />
+    </S.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 
