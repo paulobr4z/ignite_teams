7ad05fb434c88b9c974b423a0a4d763aa903c614
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-10 22:24:19 -0300

    CSS-in-JS - Utilizando o CSS Helper

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native'; 
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import theme from './src/theme';
+import theme from "./src/theme";
 
-import { Groups } from '@screens/Groups';
-import { Loading } from '@components/Loading';
+import { Loading } from "@components/Loading";
+// import { NewGroup } from "@screens/NewGroup";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar 
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 import * as S from './styles'
 
 export function Groups() {
@@ -28,6 +29,9 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
+      <Button 
+        title='Criar nova turma'
       />
     </S.Container>
   );
