1e60a8fba3dc33bffc8c2abb0f8b07845fa05e29
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-12 16:40:22 -0300

    Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,10 +5,16 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { useNavigation } from '@react-navigation/native'
 import * as S from './styles'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <S.Container>
@@ -32,6 +38,7 @@ export function Groups() {
       />
       <Button 
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
     </S.Container>
   );
