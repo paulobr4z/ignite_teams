381c959dec865d3027ff67c0333b4ebc3ae2b455
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-12 16:17:05 -0300

    Estrutura de Navegação

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 import theme from "./src/theme";
 
 import { Loading } from "@components/Loading";
-import { Players } from "@screens/Players";
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Players/> : <Loading/> }
+      { fontsLoaded ? <Routes /> : <Loading/> }
     </ThemeProvider>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = function(api) {
             '@assets': './src/assets',
             '@components': './src/components',
             '@routes': './src/routes',
+            '@routes': './src/routes',
             '@screens': './src/screens',
             '@storage': './src/storage',
             '@theme': './src/theme',
