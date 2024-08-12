e2b68cbd19f202f1c3848f852f3c2c06645402c6
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-12 18:19:11 -0300

    Local Storage - Abrindo turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import * as S from './styles'
+
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
   const navigation = useNavigation()
 
   function handleNewGroup() {
-    navigation.navigate('new')
+    navigation.navigate('new');
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  },[]))
 
   return (
     <S.Container>
@@ -28,7 +47,8 @@ export function Groups() {
         keyExtractor={item => item}
         renderItem={({ item }) => (
           <GroupCard 
-            title={item} 
+            title={item}
+            onPress={() => handleOpenGroup(item)}
           />
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
 
 import { Container, Content, Icon } from "./styles";
 
@@ -7,16 +9,31 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { groupCreate } from "@storage/group/groupCreate";
 
 export function NewGroup() {
-  const [group, setGroup] = useState('')
+  const [group, setGroup] = useState('');
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate('players', { group })
+  async function handleNew() {
+    try {
+
+      if(group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
+      await groupCreate(group);
+      navigation.navigate('players', { group });
+    } catch (error) {
+      if(error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
+    }
   }
-
   return (
     <Container>
       <Header showBackButton />
