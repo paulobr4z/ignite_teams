590dcdb2d676ac94bc7c1d23710a1a9a2340b1f4
paulobr4z <paulobrazaraujo@gmail.com>
2024-08-13 15:05:20 -0300

    finalização projeto

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,24 @@
-import { useCallback, useState } from 'react';
-import { FlatList } from 'react-native';
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GroupCard';
-import { ListEmpty } from '@components/ListEmpty';
-import { Button } from '@components/Button';
+import { useState, useCallback } from 'react';
+import { Alert, FlatList } from 'react-native';
+
 import { useNavigation, useFocusEffect } from '@react-navigation/native'
-import * as S from './styles'
 
 import { groupsGetAll } from '@storage/group/groupsGetAll';
 
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+import { Container } from './styles';
+import { Loading } from '@components/Loading';
+
 export function Groups() {
+  const [isLoading, setIsLoading] = useState(true);
   const [groups, setGroups] = useState<string[]>([]);
-  const navigation = useNavigation()
+
+  const navigation = useNavigation();
 
   function handleNewGroup() {
     navigation.navigate('new');
@@ -20,47 +26,55 @@ export function Groups() {
 
   async function fetchGroups() {
     try {
+      setIsLoading(true);
       const data = await groupsGetAll();
       setGroups(data)
     } catch (error) {
+      Alert.alert('Turmas', 'Não foi possível carregar as turmas');
       console.log(error);
-    }
+    } finally {
+      setIsLoading(false);
+    } 
   }
 
   function handleOpenGroup(group: string) {
     navigation.navigate('players', { group })
   }
-
+  
   useFocusEffect(useCallback(() => {
     fetchGroups()
   },[]))
+  
 
   return (
-    <S.Container>
+    <Container>
       <Header />
       <Highlight 
         title="Turmas"
-        subtitle="jogue com a sua turma"
+        subtitle="jogue com sua turma"
       />
-      <FlatList 
-        data={groups}
-        keyExtractor={item => item}
-        renderItem={({ item }) => (
-          <GroupCard 
-            title={item}
-            onPress={() => handleOpenGroup(item)}
+      {
+        isLoading ? <Loading /> :
+          <FlatList 
+            data={groups}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+              <GroupCard 
+                title={item}
+                onPress={() => handleOpenGroup(item)}
+              />
+            )}
+            contentContainerStyle={groups.length === 0 && { flex: 1 }}
+            ListEmptyComponent={() => (
+              <ListEmpty message="Que tal cadastrar a primeira turma?" />
+            )}
           />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty message="Que tal cadastrar a primeira turma?" />
-        )}
-      />
+      }
+
       <Button 
         title='Criar nova turma'
         onPress={handleNewGroup}
       />
-    </S.Container>
+    </Container>
   );
 }
-
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Alert } from "react-native";
+
 import { AppError } from "@utils/AppError";
+import { groupCreate } from "@storage/group/groupCreate";
 
 import { Container, Content, Icon } from "./styles";
 
@@ -9,7 +11,6 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { groupCreate } from "@storage/group/groupCreate";
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
@@ -34,13 +35,14 @@ export function NewGroup() {
       }
     }
   }
+
   return (
     <Container>
       <Header showBackButton />
 
       <Content>
         <Icon />
-
+        
         <Highlight 
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
@@ -60,5 +62,3 @@ export function NewGroup() {
     </Container>
   )
 }
-
-
